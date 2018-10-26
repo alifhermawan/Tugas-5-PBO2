f363944d26ede8e3f6e64af54ec73791d9a7e141
alifhermawan <43422583+alifhermawan@users.noreply.github.com>
2018-10-27 00:18:14 +0700

    Update Kakuzu.java

--- PBO2-10117045-Latihan60-Akatsuki/src/pbo2/pkg10117045/latihan60/akatsuki/Kakuzu.java
+++ PBO2-10117045-Latihan60-Akatsuki/src/pbo2/pkg10117045/latihan60/akatsuki/Kakuzu.java
@@ -2,25 +2,12 @@ package pbo2.pkg10117045.latihan60.akatsuki;
 
 public class Kakuzu extends Akatsuki {
 
-    private String jutsu;
-
-    public Kakuzu(String nama, String asal, String posisiCincin) {
-        super(nama, asal, posisiCincin);
+    public Kakuzu(String nama, String asal, String posisiCincin, String jutsu) {
+        super(nama, asal, posisiCincin, jutsu);
     }
 
-
-    public String getJutsu() {
-        return jutsu;
-    }
-
-    public void setJutsu(String jutsu) {
-        this.jutsu = jutsu;
-    }
-
-    @Override
     public void kematian() {
         System.out.println("Mati karena diserang oleh Naruto  dengan jutsu barunya (Rasenshuriken)");
     }
 
-    
 }
