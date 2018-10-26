5eeef82c1ad5677926f74d7895e05f36152d52be
alifhermawan <43422583+alifhermawan@users.noreply.github.com>
2018-10-27 00:18:47 +0700

    Update Akatsuki.java

--- PBO2-10117045-Latihan60-Akatsuki/src/pbo2/pkg10117045/latihan60/akatsuki/Akatsuki.java
+++ PBO2-10117045-Latihan60-Akatsuki/src/pbo2/pkg10117045/latihan60/akatsuki/Akatsuki.java
@@ -1,24 +1,34 @@
 package pbo2.pkg10117045.latihan60.akatsuki;
 
 public class Akatsuki {
+
     protected String nama;
     protected String asal;
-    protected  String posisiCincin;
+    protected String posisiCincin;
+    protected String jutsu;
 
-    public Akatsuki(String nama, String asal, String posisiCincin) {
+    public Akatsuki(String nama, String asal, String posisiCincin, String jutsu) {
         this.nama = nama;
         this.asal = asal;
         this.posisiCincin = posisiCincin;
+        this.jutsu = jutsu;
+
     }
 
+    public String getNama() {
+        return nama;
+    }
 
-    public void jutsu (){
-        System.err.println("");
+    public String getAsal() {
+        return asal;
     }
-    
-    public void kematian(){
-        System.out.println("Mati karena dibunuh");
+
+    public String getPosisiCincin() {
+        return posisiCincin;
     }
-    
-    
+
+    public String getJutsu() {
+        return jutsu;
+    }
+
 }
