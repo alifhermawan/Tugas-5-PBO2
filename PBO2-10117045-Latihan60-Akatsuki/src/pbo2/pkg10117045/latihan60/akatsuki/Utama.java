ff1c8e05f2031c682c084db210b06146b43a3670
alifhermawan <43422583+alifhermawan@users.noreply.github.com>
2018-10-27 00:16:35 +0700

    Update Utama.java

--- PBO2-10117045-Latihan60-Akatsuki/src/pbo2/pkg10117045/latihan60/akatsuki/Utama.java
+++ PBO2-10117045-Latihan60-Akatsuki/src/pbo2/pkg10117045/latihan60/akatsuki/Utama.java
@@ -11,23 +11,23 @@ package pbo2.pkg10117045.latihan60.akatsuki;
 public class Utama {
 
     public static void main(String[] args) {
-        Kakuzu ka = new Kakuzu("Kakuzu", "Takigakure", "Jari tengah kiri");
-        ka.setJutsu("Domu, Zokkoku, Gian, Atsugai, Kimen no Buken");
+        Kakuzu ka = new Kakuzu("Kakuzu", "Takigakure", "Jari tengah kiri","Domu, Zokkoku, Gian, Atsugai, Kimen no Buken");
+ 
 
-        Sasori sa = new Sasori("Sasori", "Sunagakure", "Jempol kri");
-        sa.setJutsu("Shinshin Happa,Senju Sobu, Hyakki no Soen, Satetsu Kesshu, Satetsu Kaiho");
+        Sasori sa = new Sasori("Sasori", "Sunagakure", "Jempol kri","Shinshin Happa,Senju Sobu, Hyakki no Soen, Satetsu Kesshu, Satetsu Kaiho");
+
 
         System.out.println("=== Daftar Anggota Akatsuki Yang Telah Mati ===");
         System.out.println("\n"+ka.nama);
         System.out.println("Asal : " + ka.asal);
         System.out.println("Posisi Cincin : " + ka.posisiCincin);
-        System.out.println("Jutsu : " + ka.getJutsu());
+        System.out.println("Jutsu : " + ka.jutsu);
         ka.kematian();
 
         System.out.println("\n"+sa.nama);
         System.out.println("Asal : " + sa.asal);
         System.out.println("Posisi Cincin : " + sa.posisiCincin);
-        System.out.println("Jutsu : " + sa.getJutsu());
+        System.out.println("Jutsu : " + sa.jutsu);
         sa.kematian();
 
     }
