4bea67d864fd967b69795d209ad9066658692e87
alifhermawan <43422583+alifhermawan@users.noreply.github.com>
2018-10-27 00:17:43 +0700

    Update Sasori.java

--- PBO2-10117045-Latihan60-Akatsuki/src/pbo2/pkg10117045/latihan60/akatsuki/Sasori.java
+++ PBO2-10117045-Latihan60-Akatsuki/src/pbo2/pkg10117045/latihan60/akatsuki/Sasori.java
@@ -2,21 +2,10 @@ package pbo2.pkg10117045.latihan60.akatsuki;
 
 public class Sasori extends Akatsuki {
 
-    private String jutsu;
-
-    public Sasori(String nama, String asal, String posisiCincin) {
-        super(nama, asal, posisiCincin);
+    public Sasori(String nama, String asal, String posisiCincin, String jutsu) {
+        super(nama, asal, posisiCincin, jutsu);
     }
 
-    public String getJutsu() {
-        return jutsu;
-    }
-
-    public void setJutsu(String jutsu) {
-        this.jutsu = jutsu;
-    }
-
-    @Override
     public void kematian() {
         System.out.println("Mati karena dikalahkan oleh Chino dengan menusuk jantung"
                 + " Sasori dengan menggunakan replika boneka kedua orang tua Sasori");
